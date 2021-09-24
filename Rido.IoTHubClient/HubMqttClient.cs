1fcd18194839fd54fa5db783108c93b5af034133
ridomin <ridomin@live.com>
2021-09-24 13:43:13 -0700

    first release

---
+++ Rido.IoTHubClient/HubMqttClient.cs
@@ -0,0 +1,235 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+using MQTTnet.Client.Publishing;
+using MQTTnet.Client.Subscribing;
+using MQTTnet.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Rido.IoTHubClient
+{
+    internal class HubMqttClient
+    {
+
+        public class CommandEventArgs : EventArgs
+        {
+            public string Rid { get; set; }
+            public string CommandName { get; set; }
+            public string CommandRequestMessageJson { get; set; }
+            public string Topic { get; set; }
+        }
+
+        public class PropertyEventArgs : EventArgs
+        {
+            public string Rid { get; set; }
+            public string PropertyMessageJson { get; set; }
+            public string Topic { get; set; }
+            public int Version { get; set; }
+        }
+
+        public class HubMqttClient
+        {
+            public event EventHandler<CommandEventArgs> OnCommandReceived;
+            public event EventHandler<PropertyEventArgs> OnPropertyReceived;
+            public event EventHandler<MqttApplicationMessageReceivedEventArgs> OnMessageReceived;
+
+            public IMqttClient MqttClient;
+            public string ClientId { get; set; }
+
+            // Dictionary<int, Func<string, string>> callbacks = new Dictionary<int, Func<string,string>>();
+            static Action<string> cb;
+            static Action<int> patch_cb;
+            private int lastRid = 1;
+            public HubMqttClient(IMqttClient c, string clientId)
+            {
+                MqttClient = c;
+                ClientId = clientId;
+
+            }
+
+            public async Task SendTelemetryAsync(object payload) => await PublishAsync("$az/iot/telemetry", payload);
+            public async Task SendTelemetryAsync(string payload) => await PublishAsync("$az/iot/telemetry", payload);
+
+            public async Task<MqttClientPublishResult> RequestTwinAsync(Action<string> GetTwinCallback)
+            {
+                var puback = await PublishAsync($"$az/iot/twin/get/?rid={lastRid++}", string.Empty);
+                //callbacks.Add(lastRid, GetTwinCallback);
+                cb = GetTwinCallback;
+                return puback;
+            }
+
+
+            public async Task<MqttClientPublishResult> UpdateTwinAsync(object payload, Action<int> patchTwinCallback)
+            {
+                var puback = await PublishAsync($"$az/iot/twin/patch/reported/?rid={lastRid++}", payload);
+                patch_cb = patchTwinCallback;
+                return puback;
+            }
+
+            public async Task<MqttClientPublishResult> UpdateTwinAsync(string payload, Action<int> patchTwinCallback)
+            {
+                var puback = await PublishAsync($"$az/iot/twin/patch/reported/?rid={lastRid++}", payload);
+                patch_cb = patchTwinCallback;
+                return puback;
+            }
+
+
+            public async Task CommandResponseAsync(string rid, string cmdName, object payload, string status) =>
+                await PublishAsync($"$az/iot/methods/{cmdName}/response/?rid={rid}&rc={status}", payload);
+            public async Task CommandResponseAsync(string rid, string cmdName, string payload, string status) =>
+                await PublishAsync($"$az/iot/methods/{cmdName}/response/?rid={rid}&rc={status}", payload);
+
+            public static async Task<HubMqttClient> CreateWithClientCerts(string hostname, string certPath, string certPwd)
+            {
+                using var cert = new X509Certificate2(certPath, certPwd);
+                var cid = cert.Subject.Substring(3);
+                List<X509Certificate> certs = new List<X509Certificate> { cert };
+
+                var factory = new MqttFactory();
+                var mqttClient = factory.CreateMqttClient();
+                var hub = new HubMqttClient(mqttClient, cid);
+
+                var username = $"av=2021-06-30-preview&h={hostname}&did={cid}&am=X509&dtmi=dtmi:aa:b;1";
+                //var username = $"{hostname}.azure-devices.net/{cid}/?api-version=2020-09-30&DeviceClientType=RidoTests&x509=true";
+                Console.WriteLine(username);
+                Console.WriteLine($"{cert.SubjectName.Name} issued by {cert.IssuerName.Name} NotAfter {cert.GetExpirationDateString()} ({cert.Thumbprint})");
+                var options = new MqttClientOptionsBuilder()
+                    .WithClientId(cid)
+                    .WithTcpServer(hostname, 8883)
+                    .WithTls(new MqttClientOptionsBuilderTlsParameters
+                    {
+                        UseTls = true,
+                        SslProtocol = System.Security.Authentication.SslProtocols.Tls12,
+                        Certificates = certs
+                    })
+                    .WithCredentials(new MqttClientCredentials()
+                    {
+                        Username = username,
+                        Password = Encoding.UTF8.GetBytes("")
+                    })
+                    .Build();
+
+                mqttClient.UseApplicationMessageReceivedHandler(e =>
+                {
+                    string msg = string.Empty;
+
+                    var segments = e.ApplicationMessage.Topic.Split('/');
+                    int rid = 0;
+                    int twinVersion = 0;
+                    if (e.ApplicationMessage.Topic.Contains("?"))
+                    {
+                        // parse qs to extract the rid
+                        var qs = HttpUtility.ParseQueryString(segments[segments.Length - 1]);
+                        rid = Convert.ToInt32(qs["rid"]);
+                        twinVersion = Convert.ToInt32(qs["v"]);
+                    }
+
+                    if (e.ApplicationMessage.Payload != null)
+                    {
+                        msg = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+                    }
+
+
+                    if (e.ApplicationMessage.Topic.StartsWith("$az/iot/twin"))
+                    {
+                        Console.WriteLine($"<- {e.ApplicationMessage.Topic}  {e.ApplicationMessage.Payload?.Length} Bytes");
+                        if (e.ApplicationMessage.Topic.StartsWith("$az/iot/twin/get/response"))
+                        {
+                            cb(msg);
+                        }
+                        else if (e.ApplicationMessage.Topic.StartsWith("$az/iot/twin/events/desired-changed"))
+                        {
+                            hub.OnPropertyReceived?.Invoke(hub, new PropertyEventArgs()
+                            {
+                                Topic = e.ApplicationMessage.Topic,
+                                Rid = rid.ToString(),
+                                PropertyMessageJson = msg,
+                                Version = twinVersion
+                            });
+                        }
+                        else if (e.ApplicationMessage.Topic.StartsWith("$az/iot/twin/patch/response/"))
+                        {
+                            patch_cb(twinVersion);
+                        }
+                    }
+                    else if (e.ApplicationMessage.Topic.StartsWith("$az/iot/methods"))
+                    {
+                        var cmdName = segments[3];
+                        Console.WriteLine($"<- {e.ApplicationMessage.Topic} {cmdName} {e.ApplicationMessage.Payload.Length} Bytes");
+                        hub.OnCommandReceived?.Invoke(hub, new CommandEventArgs()
+                        {
+                            Topic = e.ApplicationMessage.Topic,
+                            Rid = rid.ToString(),
+                            CommandName = cmdName,
+                            CommandRequestMessageJson = msg
+                        });
+                    }
+                    else
+                    {
+                        Console.WriteLine($"<- {e.ApplicationMessage.Topic} {e.ApplicationMessage.Payload?.Length} Bytes");
+                        hub.OnMessageReceived?.Invoke(hub, e);
+                    }
+                });
+
+                mqttClient.UseConnectedHandler(async e =>
+                {
+                    Console.WriteLine("### CONNECTED WITH SERVER ###");
+                    var subres = await mqttClient.SubscribeAsync(new MqttClientSubscribeOptionsBuilder()
+                                                            .WithTopicFilter("$az/iot/methods/+/+", MqttQualityOfServiceLevel.AtLeastOnce)
+                                                            .WithTopicFilter("$az/iot/twin/get/response/+", MqttQualityOfServiceLevel.AtLeastOnce)
+                                                            .WithTopicFilter("$az/iot/twin/patch/response/+", MqttQualityOfServiceLevel.AtLeastOnce)
+                                                            .WithTopicFilter("$az/iot/twin/events/desired-changed/+", MqttQualityOfServiceLevel.AtLeastOnce)
+                                                            .Build());
+                    subres.Items.ToList().ForEach(x => Console.WriteLine($"+ {x.TopicFilter.Topic} {x.ResultCode}"));
+                });
+
+                mqttClient.UseDisconnectedHandler(e =>
+                {
+                    Console.WriteLine("## DISCONNECT ##");
+                    Console.WriteLine($"** {e.ClientWasConnected} {e.Reason}");
+                });
+
+                await mqttClient.ConnectAsync(options, CancellationToken.None);
+
+                return hub;
+            }
+
+            public async Task SubscribeAsync(string topic)
+            {
+                var suback = await MqttClient.SubscribeAsync(new MqttClientSubscribeOptionsBuilder().WithTopicFilter(topic).Build());
+                suback.Items.ToList().ForEach(x => Console.WriteLine($"+ {x.TopicFilter.Topic} {x.ResultCode}"));
+            }
+
+            public async Task<MqttClientPublishResult> PublishAsync(string topic, object payload)
+            {
+                string jsonPayload = JsonSerializer.Serialize(payload);
+                return await PublishAsync(topic, jsonPayload);
+            }
+
+            public async Task<MqttClientPublishResult> PublishAsync(string topic, string payload)
+            {
+                var message = new MqttApplicationMessageBuilder()
+                                .WithTopic(topic)
+                                .WithPayload(payload)
+                                .Build();
+
+                var pubRes = await MqttClient.PublishAsync(message, CancellationToken.None);
+                if (pubRes.ReasonCode != MqttClientPublishReasonCode.Success)
+                {
+                    Console.WriteLine(pubRes.ReasonCode + pubRes.ReasonString);
+                }
+                Console.WriteLine($"-> {topic} {message.Payload?.Length} Bytes");
+                return pubRes;
+            }
+        }
+
+    }
+}
