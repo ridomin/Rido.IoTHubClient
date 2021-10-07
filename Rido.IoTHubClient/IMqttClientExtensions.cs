66e544cf5c4d7760761795e4a307f12221f4d1b1
rido-min <rido-min@users.noreply.github.com>
2021-10-07 12:05:19 -0700

    add v2 support for broker

--- Rido.IoTHubClient/IMqttClientExtensions.cs
+++ Rido.IoTHubClient/IMqttClientExtensions.cs
@@ -26,7 +26,42 @@ namespace Rido.IoTHubClient
                  .Build());
         }
 
+        public static async Task<MqttClientAuthenticateResult> ConnectV2WithSasAsync(this IMqttClient mqttClient, string hostName, string deviceId, string sasKey, int minutes)
+        {
+            (string username, byte[] password) = SasAuthV2.GenerateHubSasCredentials(hostName, deviceId, sasKey, minutes);
+            return await mqttClient.ConnectAsync(new MqttClientOptionsBuilder()
+                 .WithClientId(deviceId)
+                 .WithTcpServer(hostName, 8883)
+                 .WithCredentials(username, password)
+                 .WithTls(new MqttClientOptionsBuilderTlsParameters
+                 {
+                     UseTls = true,
+                     SslProtocol = SslProtocols.Tls12
+                 })
+                 .Build());
+        }
+
         public static async Task<MqttClientAuthenticateResult> ConnectWithX509Async(this IMqttClient mqttClient, string hostName, X509Certificate cert)
+        {
+            return await mqttClient.ConnectAsync(
+               new MqttClientOptionsBuilder()
+                   .WithClientId(cert.Subject.Substring(3))
+                   .WithTcpServer(hostName, 8883)
+                   .WithCredentials(new MqttClientCredentials()
+                   {
+                       Username = SasAuth.GetUserName(hostName, cert.Subject.Substring(3))
+                   })
+                   .WithTls(new MqttClientOptionsBuilderTlsParameters
+                   {
+                       UseTls = true,
+                       SslProtocol = SslProtocols.Tls12,
+                       Certificates = new List<X509Certificate> { cert }
+                   })
+                   .Build(),
+               CancellationToken.None);
+        }
+
+        public static async Task<MqttClientAuthenticateResult> ConnectV2WithX509Async(this IMqttClient mqttClient, string hostName, X509Certificate cert)
         {
             return await mqttClient.ConnectAsync(
                new MqttClientOptionsBuilder()
---
+++ Rido.IoTHubClient/SasAuthV2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rido.IoTHubClient
+{
+    internal class SasAuthV2
+    {
+        internal static string GetUserName(string hostName, string deviceId, string expiryString) => 
+            $"av=2021-06-30-preview&h={hostName}&did={deviceId}&am=SAS&se={expiryString}";
+        static byte[] CreateSasToken(string resource, string sasKey, string expiry)
+        {
+            static byte[] Sign(string requestString, string key)
+            {
+                using var algorithm = new System.Security.Cryptography.HMACSHA256(Convert.FromBase64String(key));
+                return algorithm.ComputeHash(Encoding.UTF8.GetBytes(requestString));
+            }
+            return Sign($"{resource}\n\n\n{expiry}\n", sasKey);
+            //return $"SharedAccessSignature sr={resource}&sig={sig}&se={expiry}";
+        }
+
+        internal static (string username, byte[] password) GenerateHubSasCredentials(string hostName, string deviceId, string sasKey, int minutes)
+        {
+            var expiry = DateTimeOffset.UtcNow.AddMinutes(minutes).ToUnixTimeMilliseconds().ToString();
+            string username = GetUserName(hostName, deviceId, expiry);
+            byte[] password = CreateSasToken($"{hostName}\n{deviceId}", sasKey, expiry);
+            return (username, password);
+        }
+    }
+}
