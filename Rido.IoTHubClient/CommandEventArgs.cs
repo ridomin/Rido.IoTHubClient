3795563727f5c108e45747c23862507116e91cce
rido-min <rido-min@users.noreply.github.com>
2021-10-13 13:27:15 -0700

    add event args again

---
+++ Rido.IoTHubClient/CommandEventArgs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rido.IoTHubClient
+{
+    public class CommandEventArgs : EventArgs
+    {
+        public string Rid { get; set; }
+        public string CommandName { get; set; }
+        public string CommandRequestMessageJson { get; set; }
+        public string Topic { get; set; }
+    }
+}
