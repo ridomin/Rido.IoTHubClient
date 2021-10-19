c8711d5357c4629ecb2da5821d0f2ddb14860607
rido-min <rido-min@users.noreply.github.com>
2021-10-19 11:22:59 -0700

    headers wip

--- Rido.IoTHubClient.Tests/IMqttClientExtensionsFixture.cs
+++ Rido.IoTHubClient.Tests/IMqttClientExtensionsFixture.cs
@@ -4,6 +4,7 @@ using MQTTnet.Client.Connecting;
 using System;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -34,6 +35,24 @@ namespace Rido.IoTHubClient.Tests
             var connack = await mqttClient.ConnectWithX509Async(hostname, new X509Certificate("testdevice.pfx", "1234"));
             Assert.Equal(MqttClientConnectResultCode.Success, connack.ResultCode);
         }
+
+        [Fact]
+        public async Task SendTelemetryWithHeaders()
+        {
+            var connack = await mqttClient.ConnectWithSasAsync(hostname, deviceId, DefaultKey, "dmit:com:example:Thermostat;1", 60);
+            Assert.Equal(MqttClientConnectResultCode.Success, connack.ResultCode);
+            var msg = new MqttApplicationMessage();
+            msg.ContentType = "application/json";
+            msg.UserProperties = new System.Collections.Generic.List<MQTTnet.Packets.MqttUserProperty>();
+            msg.UserProperties.Add(new MQTTnet.Packets.MqttUserProperty("myUserProperty", "my_comp"));
+            msg.Payload = System.Text.Encoding.UTF8.GetBytes(JsonSerializer.Serialize(new { temperature = 3 }));
+            msg.Topic = $"devices/{deviceId}/messages/events/";
+            var puback = await mqttClient.PublishAsync(msg);
+            Console.WriteLine(puback.ReasonCode);
+        }
+
+
+
         public void Dispose()
         {
             _ = mqttClient.DisconnectAsync();
