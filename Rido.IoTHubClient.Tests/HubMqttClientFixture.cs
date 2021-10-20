10f72175c9c0c59321bf52591079359e7bed6cf8
Rido <rido-min@users.noreply.github.com>
2021-10-19 23:10:20 -0700

    Feat/connection (#20)
    * add disconnect event
    
    * bump 0.0.2
    
    * add disconnect to readme
    
    * fix tests

--- Rido.IoTHubClient.Tests/HubMqttClientFixture.cs
+++ Rido.IoTHubClient.Tests/HubMqttClientFixture.cs
@@ -12,8 +12,8 @@ namespace Rido.IoTHubClient.Tests
         readonly RegistryManager rm;
         const string hubConnectionString = "HostName=tests.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=P5LfPNpLhLD/qJVOCTpuKXLi/9rmGqvkleB0quXxkws=";
         const string hubName = "tests.azure-devices.net";
-        readonly string deviceId = "d" + new Random().Next(10);
-        readonly Device device;
+        string deviceId = String.Empty;
+        Device device;
 
         private readonly ITestOutputHelper output;
 
@@ -22,6 +22,7 @@ namespace Rido.IoTHubClient.Tests
             this.output = output;
             // var tokenCredential = new DefaultAzureCredential();
             rm = RegistryManager.CreateFromConnectionString(hubConnectionString);
+            deviceId = "testdevice" + new Random().Next(10);
             device = GetOrCreateDeviceAsync(deviceId).Result;
         }
 
@@ -134,7 +135,7 @@ namespace Rido.IoTHubClient.Tests
             string modelId = "dtmi:rido:test;1";
             IHubMqttClient client = await HubMqttClient.CreateWithClientCertsAsync(hubName, new X509Certificate2("testdevice.pfx", "1234"), modelId);
             Assert.True(client.IsConnected);
-            var deviceRecord = await rm.GetTwinAsync(device.Id);
+            var deviceRecord = await rm.GetTwinAsync("testdevice");
             Assert.Equal(modelId, deviceRecord.ModelId);
             await client.CloseAsync();
         }
@@ -213,7 +214,19 @@ namespace Rido.IoTHubClient.Tests
             await client.CloseAsync();
         }
 
-
+        [Fact]
+        public async Task ConnectSameDeviceTwiceTriggersDisconnect()
+        {
+            var device= await GetOrCreateDeviceAsync("fakeDevice");
+            var client1 = await HubMqttClient.CreateAsync(hubName, device.Id, device.Authentication.SymmetricKey.PrimaryKey);
+            Assert.True(client1.IsConnected);
+            bool hasDisconnected = false;
+            client1.OnMqttClientDisconnected += (o, e) => hasDisconnected = true;
+            var client2 = await HubMqttClient.CreateAsync(hubName, device.Id, device.Authentication.SymmetricKey.PrimaryKey);
+            Assert.True(client2.IsConnected);
+            await Task.Delay(500);
+            Assert.True(hasDisconnected);
+        }
         private async Task<Device> GetOrCreateDeviceAsync(string deviceId, bool x509 = false)
         {
             var device = await rm.GetDeviceAsync(deviceId);
