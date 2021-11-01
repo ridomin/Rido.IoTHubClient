ce9402abf3769983e2ce6f123343b4a56a13b7e5
rido-min <rido-min@users.noreply.github.com>
2021-11-01 00:40:41 -0700

    undo using

--- Rido.IoTHubClient/TaskTimeoutExtension.cs
+++ Rido.IoTHubClient/TaskTimeoutExtension.cs
@@ -9,16 +9,18 @@ namespace Rido.IoTHubClient
     {
         public static async Task<TResult> TimeoutAfter<TResult>(this Task<TResult> task, TimeSpan timeout)
         {
-            using var timeoutCancellationTokenSource = new CancellationTokenSource();
-            var completedTask = await Task.WhenAny(task, Task.Delay(timeout, timeoutCancellationTokenSource.Token));
-            if (completedTask == task)
+            using (var timeoutCancellationTokenSource = new CancellationTokenSource())
             {
-                timeoutCancellationTokenSource.Cancel();
-                return await task;
-            }
-            else
-            {
-                throw new TimeoutException($"{nameof(TimeoutAfter)}: The operation has timed out after {timeout:mm\\:ss}");
+                var completedTask = await Task.WhenAny(task, Task.Delay(timeout, timeoutCancellationTokenSource.Token));
+                if (completedTask == task)
+                {
+                    timeoutCancellationTokenSource.Cancel();
+                    return await task;
+                }
+                else
+                {
+                    throw new TimeoutException($"{nameof(TimeoutAfter)}: The operation has timed out after {timeout:mm\\:ss}");
+                }
             }
 
         }
