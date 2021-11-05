53681a3c3e5785dcde6a526aeea2aac32c09fa97
Rido <ridomin@live.com>
2021-11-05 07:08:39 -0700

    Split Connection and Client (#36)
    * intrduce connection
    
    * before refactor tests
    
    * green
    
    * clean up
    
    * debugging refresh token
    
    * split interfaces
    
    * dont unsubcribe on close
    
    Co-authored-by: rido-min <rido-min@users.noreply.github.com>

---
+++ Rido.IoTHubClient/IHubMqttConnection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Rido.IoTHubClient
+{
+    public interface IHubMqttConnection
+    {
+        ConnectionSettings ConnectionSettings { get; }
+        bool IsConnected { get; }
+        event EventHandler<DisconnectEventArgs> OnMqttClientDisconnected;
+        Task CloseAsync();
+
+    }
+}
